56bf2fe3c49585fe6f09064f1bc9df6a673f5c45
Wayne <146171587+wayne-spec@users.noreply.github.com>
2024-12-28 01:49:25 +0000

    Initial Commit

---
+++ Job-Board/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./inex.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
